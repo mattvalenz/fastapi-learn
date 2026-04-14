5b9f6a45e8e2b7afdd17183271657e84def17816
Matheus Valenzuela <matheuszvalenz@gmail.com>
2026-04-14 18:03:11 +0100

    \\

--- main.py
+++ main.py
@@ -56,10 +56,22 @@ def get_post(id: int, response: Response):
     #     return {"message": f"post with id: {id} was not found"}
     return {"post_detail" : post}
 
-@app.delete("/posts/{id}")
-def delete_post():
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
     index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist.")
     my_posts.pop(index)
     
-    return {'message':'post successfully deleted'}
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"post with id: {id} does not exist.")
+    
+    post_dict = post.model_dump()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {'data': post_dict}
