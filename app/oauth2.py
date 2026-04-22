5b8c6353c4c15d78b88ed9d925bdec79179cf809
Matheus Valenzuela <matheuszvalenz@gmail.com>
2026-04-22 17:05:14 +0100

    working on oauth2, handling JWT token

---
+++ app/oauth2.py
@@ -0,0 +1,16 @@
+import jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY =""
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    
+    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire.timestamp()})
+    
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    
+    return encoded_jwt
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
 
-from .. import database, schemas, models, utils
+from .. import database, schemas, models, utils, oauth2
+
 
 
 router =APIRouter(tags=['Authentication'])
@@ -18,5 +19,7 @@ def login(user_credentials: schemas.UserLogin,db: Session = Depends(database.get
     if not utils.verify(user_credentials.password, user.password):
         raise HTTPException(status_code=404, detail="Invalid credentials")
     
-    return {"token": "example token"}
+    access_token = oauth2.create_access_token(data= {"user_id": user.id})
+    
+    return {"access_token": access_token, "token_type" : "bearer"}
 
