4cb09754310d32281a44f08e1e63addde5149b42
Matheus Valenzuela <matheuszvalenz@gmail.com>
2026-05-03 12:37:44 +0100

    changed the logic for authenticating to store id as integers

--- app/oauth2.py
+++ app/oauth2.py
@@ -18,7 +18,7 @@ def create_access_token(data: dict):
     expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire.timestamp()})
     
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=[ALGORITHM])
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     
     return encoded_jwt
 
@@ -26,9 +26,9 @@ def verify_access_token(token: str, credentials_exception):
     
     try:
         
-        payload = jwt.decode(token, SECRET_KEY, algorithms= ALGORITHM)
+        payload = jwt.decode(token, SECRET_KEY, algorithms= [ALGORITHM])
     
-        id: str = payload.get("user_id")
+        id: int = payload.get("user_id")
     
         if id is None:
             raise credentials_exception
