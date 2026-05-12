2515d62ca0a1b9af28aa25a513b4d38cff676b43
Matheus Valenzuela <matheuszvalenz@gmail.com>
2026-05-12 21:39:02 +0100

    working on many to one relation (user id matching post creation)

--- app/routers/post.py
+++ app/routers/post.py
@@ -31,7 +31,7 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), get_cu
     
     # conn.commit()
     
-    new_post = models.Post(**post.model_dump())
+    new_post = models.Post(owner_id = current_user.id, **post.model_dump())
     print(current_user.email)
     db.add(new_post)
     db.commit()
