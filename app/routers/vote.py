643ecf3aee89537fa2c0de7b20d6747558731a5b
Matheus <matheuszvalenz@gmail.com>
2026-06-06 15:38:00 +0100

    adding logic for non existant post id for votes

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -1,3 +1,5 @@
+from curses.ascii import HT
+
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from .. import schemas, database, models, oauth2
 from sqlalchemy.orm import Session
@@ -11,6 +13,14 @@ def vote(
     db: Session = Depends(database.get_db),
     current_user: int = Depends(oauth2.get_current_user),
 ):
+
+    post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with id: {vote.post_id} does not exist.",
+        )
 
     vote_query = db.query(models.Vote).filter(
         models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id
